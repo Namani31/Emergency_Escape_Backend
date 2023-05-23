f6c9f2d7fe823dbfdd08bf43b033664eee3435ec
YJ <kimyunji9272@gmail.com>
2023-05-23 22:06:52 +0900

    FEAT: 테이블 구조 변경에 의한 엔티티 수정

--- src/main/java/com/namani/demo/dto/LocationPostRequestDto.java
+++ src/main/java/com/namani/demo/dto/LocationPostRequestDto.java
@@ -3,23 +3,7 @@ package com.namani.demo.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class LocationPostRequestDto {
-    int id;
-    @JsonProperty("location")
-    Location location;
 
-    public int getId() {
-        return id;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setLocation(Location location) {
-        this.location = location;
-    }
+    String qrData;
+    BeaconData beaconData;
 }
